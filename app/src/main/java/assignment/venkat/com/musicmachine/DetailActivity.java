0930b7c7c154e338e46fec402d9c0161b25ba871
Venkat Gonuguntala <venkyg88@gmail.com>
2016-07-26 18:18:22 -0400

    sharing data among two different apps using implicit intents also look for the CustomIntentTesterApp for your reference. It is the invoked App with the data. Chances are there to get the unmarshalling exception if the package name of the parcelable object is not same in two apps

--- app/src/main/java/assignment/venkat/com/musicmachine/DetailActivity.java
+++ app/src/main/java/assignment/venkat/com/musicmachine/DetailActivity.java
@@ -2,11 +2,13 @@ package assignment.venkat.com.musicmachine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import assignment.venkat.com.musicmachine.model.Song;
@@ -18,7 +20,9 @@ import assignment.venkat.com.musicmachine.model.Song;
 public class DetailActivity extends AppCompatActivity {
 
     private Song mSong;
+    public static final String SHARE_SONG = "assignment.venkat.intent.action.SHARE_SONG";
 
+    private RelativeLayout mRelativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,30 +30,45 @@ public class DetailActivity extends AppCompatActivity {
 
         TextView titleLabel = (TextView) findViewById(R.id.songTitleLabel);
         final CheckBox favoriteCheckbox = (CheckBox) findViewById(R.id.checkBox);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.rootLayout);
 
         Intent intent = getIntent();
         /*if (intent.getStringExtra(MainActivity.EXTRA_SONG) != null) {
             String songTitle = intent.getStringExtra(MainActivity.EXTRA_SONG);
             titleLabel.setText(songTitle);
         }*/
-        if (intent.getParcelableExtra(MainActivity.EXTRA_SONG) != null) {
-            Song song = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
-            titleLabel.setText(song.getTitle());
-            favoriteCheckbox.setChecked(song.isFavorite());
+
+        if (Intent.ACTION_SEND.equals(intent.getAction())){
+            handleSendIntent(intent);
+
+        } else {
+            if (intent.getParcelableExtra(MainActivity.EXTRA_SONG) != null) {
+                mSong = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
+                titleLabel.setText(mSong.getTitle());
+                favoriteCheckbox.setChecked(mSong.isFavorite());
+            }
+
+            final int listPosition = intent.getIntExtra(MainActivity.EXTRA_LIST_POSITION, 0);
+
+            favoriteCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    Intent resultIntent = new Intent();
+                    resultIntent.putExtra(MainActivity.EXTRA_FAVORITE, isChecked);
+                    resultIntent.putExtra(MainActivity.EXTRA_LIST_POSITION, listPosition);
+                    setResult(RESULT_OK, resultIntent);
+                    finish();
+                }
+            });
+        }
+    }
+
+    private void handleSendIntent(Intent intent) {
+        if (intent.getStringExtra(Intent.EXTRA_TEXT) != null) {
+            Snackbar.make(mRelativeLayout, intent.getStringExtra(Intent.EXTRA_TEXT),
+                                Snackbar.LENGTH_LONG).show();
         }
 
-        final int listPosition = intent.getIntExtra(MainActivity.EXTRA_LIST_POSITION, 0);
-
-        favoriteCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Intent resultIntent =  new Intent();
-                resultIntent.putExtra(MainActivity.EXTRA_FAVORITE, isChecked);
-                resultIntent.putExtra(MainActivity.EXTRA_LIST_POSITION, listPosition);
-                setResult(RESULT_OK, resultIntent);
-                finish();
-            }
-        });
     }
 
     @Override
@@ -62,6 +81,15 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
+
+        if (itemId == R.id.action_share) {
+            if (mSong != null) {
+                Intent customIntent = new Intent(SHARE_SONG);
+                customIntent.putExtra(MainActivity.EXTRA_SONG, mSong);
+                Intent chooser = Intent.createChooser(customIntent, "share song");
+                startActivity(chooser);
+            }
+        }
 
         return super.onOptionsItemSelected(item);
     }
